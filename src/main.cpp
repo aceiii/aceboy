0dcd5c03b8fe5586860172f36d5d2fe341bb9f10
Borin Ouch <borin.ouch@gmail.com>
2024-04-28 22:06:26 -0700

    Add more dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
+#include <argparse/argparse.hpp>
+#include <magic_enum.hpp>
 #include <spdlog/spdlog.h>
 
-int main() {
-  spdlog::info("Hello, world!!");
+static bool set_logging_level(const std::string &level_name) {
+  auto level = magic_enum::enum_cast<spdlog::level::level_enum>(level_name);
+  if (level.has_value()) {
+    spdlog::set_level(level.value());
+    return true;
+  }
+  return false;
+}
+
+auto main(int argc, char *argv[]) -> int {
+  spdlog::set_level(spdlog::level::info);
+
+  argparse::ArgumentParser program("aceboy", "0.0.1");
+
+  program.add_argument("--log-level")
+      .help("Set the verbosity for logging")
+      .default_value(std::string("info"))
+      .nargs(1);
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception &err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    return 1;
+  }
+
+  const std::string level = program.get("--log-level");
+  if (!set_logging_level(level)) {
+    std::cerr << fmt::format("Invalid argument \"{}\" - allowed options: "
+                             "{{trace, debug, info, warn, err, critical, off}}",
+                             level)
+              << std::endl;
+    std::cerr << program;
+    return 1;
+  }
+
+  spdlog::info("Exiting.");
+
   return 0;
 }
