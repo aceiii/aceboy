94ced49754d3fdd2bb9fe1e3d8e2f737d1e36478
Borin Ouch <borin.ouch@gmail.com>
2024-05-13 23:52:38 -0700

    Add decoder class

--- src/decoder.h
+++ src/decoder.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "instructions.h"
+#include "registers.h"
+#include <expected>
+
+using decode_result = std::expected<Instruction, std::string>;
+
+class Decoder {
+public:
+  Decode(const uint8_t* memory, const Registers* registers);
+
+  decode_result decode();
+};
--- src/instructions.h
+++ src/instructions.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "opcodes.h"
+
 #include <optional>
 #include <string_view>
-
 
 enum Cond {
   kCondNZ,
@@ -12,7 +13,7 @@ enum Cond {
 };
 
 struct Instruction {
-  std::string_view mnemonic;
+  Opcode opcode;
   size_t cycles;
   size_t bytes;
 };
